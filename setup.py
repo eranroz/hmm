6bb0fd43f7970fe37d49e964ef87b29a94565518
Eran Rosenthal <eranroz@cs.huji.ac.il>
2015-12-27 15:47:39 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
 setup(
     name='hmm_kit',
@@ -10,9 +11,10 @@ setup(
     url='https://github.com/eranroz/hmm',
     license='MIT License',
     packages=['hmm_kit'],
-    ext_modules=cythonize(["hmm_kit/_hmmc.pyx"]),
+    ext_modules=cythonize(['hmm_kit/_hmmc.pyx']),
     requires=['cython', 'matplotlib'],
     scripts=['scripts/simple_hmm.py'],
+    build_ext=build_ext,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Development Status :: 4 - Beta',
